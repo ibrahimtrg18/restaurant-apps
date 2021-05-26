ba3034f9723c914de62a54d967e3af596a3c7df0
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-05-27 05:51:37 +0700

    improving ui rating

--- src/scripts/components/restaurant-item.js
+++ src/scripts/components/restaurant-item.js
@@ -7,13 +7,19 @@ class RestaurantItem extends HTMLElement {
   render() {
     this.innerHTML = `
       <div class="restaurant-item">
-        <img
-          class="restaurant-item__image"
-          src="${this._restaurant.pictureId}"
-        />
-        <div class="restaurant-item__location">${this._restaurant.city}</div>
-        <div class="restaurant-item__info">
-          <h2 class="info__name"><a href="${this._restaurant.id}">${this._restaurant.name}</a></h2>
+        <div class="restaurant-item__head">
+          <img
+            class="restaurant-item__image"
+            src="${this._restaurant.pictureId}"
+          />
+          <div class="restaurant-item__location">${this._restaurant.city}</div>
+        </div>
+
+        <div class="restaurant-item__body">
+          <span class="restaurant-item__rating">Rating: ${this._restaurant.rating}</span>
+          <h2 class="info__name">
+            <a href="${this._restaurant.id}">${this._restaurant.name}</a>
+          </h2>
           <p class="info__description">${this._restaurant.description}</p>
         </div>
       </div>
