136b6ab2f78fd4744982c6e2f86dee7660422b82
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-07-25 13:52:25 +0700

    testing jest for the first time

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev',
-  CACHE_NAME: new Date().toISOString(),
+  CACHE_NAME: 'RESTAURANT-APP-V1',
   DATABASE_NAME: 'restaurant-app-database',
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: 'restaurants',
